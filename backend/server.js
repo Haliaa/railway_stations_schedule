10f30c30973acfd1531065db3240d76291434731
Halia <91005548+Haliaa@users.noreply.github.com>
2024-06-07 16:35:41 +0300

    update sequence

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(expressFileUpload());
 app.use(cors());
 
+const path = require("path");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 // Route setup
 app.use("/trains", trainsRouter);
 app.use("/stations", stationsRouter);
 app.use("/schedules", schedulesRouter);
+
+// Catch-all route to serve the React frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -39,16 +49,6 @@ app.use((err, req, res, next) => {
     error: err.message || "Unknown error",
     code: err.status || 400,
   });
-});
-
-const path = require("path");
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // Catch-all route for 404 errors
